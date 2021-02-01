caff05bd70cffa6039bfe089c7bc3029647679e7
Chandler Squires <csquires@mit.edu>
2021-01-31 19:45:16 -0500

    issues also on linux

--- dev_setup.sh
+++ dev_setup.sh
@@ -2,5 +2,5 @@
 
 python3 -m venv venv
 source venv/bin/activate
-pip3 install networkx ipython numpy scipy ipdb tqdm twine wheel
+pip3 install networkx ipython numpy scipy ipdb tqdm twine wheel sphinx_rtd_theme
 pip3 install jedi==0.17.2
