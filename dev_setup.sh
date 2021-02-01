c027de46f6bf50f7245d9db46a80bbc6fadfe983
Chandler <chandlersquires@gmail.com>
2021-02-01 01:11:21 -0500

    travis installs

--- dev_setup.sh
+++ dev_setup.sh
@@ -2,5 +2,6 @@
 
 python3 -m venv venv
 source venv/bin/activate
-pip3 install networkx ipython numpy scipy ipdb tqdm twine wheel sphinx_rtd_theme
+pip3 install networkx numpy scipy ipdb tqdm
+pip3 install ipython twine wheel sphinx_rtd_theme coverage
 pip3 install jedi==0.17.2
